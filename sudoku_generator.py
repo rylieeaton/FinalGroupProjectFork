b878c4ed0254d37f2cf51b7641ba03b8539c8568
golbe <amitgolberg@gmail.com>
2025-11-25 14:18:14 -0500

    Wrote constructor

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -23,7 +23,10 @@ class SudokuGenerator:
 	None
     '''
     def __init__(self, row_length, removed_cells):
-        pass
+        self.row_length = row_length
+        self.removed_cells = removed_cells
+        self.board = []
+        self.box_length = row_length ** 0.5
 
     '''
 	Returns a 2D python list of numbers which represents the board
