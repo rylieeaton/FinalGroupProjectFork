e713a79cf49f7098dd65ae5c98b4c7f26316249b
golbe <amitgolberg@gmail.com>
2025-11-25 14:33:03 -0500

    is_valid

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -105,7 +105,9 @@ class SudokuGenerator:
 	Return: boolean
     '''
     def is_valid(self, row, col, num):
-        pass
+        if self.valid_in_col(col, num) and self.valid_in_row(row, num) and self.valid_in_box(row, col, num):
+            return True
+        return False
 
     '''
     Fills the specified 3x3 box with values
