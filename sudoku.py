5cfdac4c162f279e0057d5ac2cdd551ef81cb8f7
Bread <rylie.eaton@gmail.com>
2025-11-29 19:50:19 -0500

    started the code needed to play the game; needs fixes; kinda ugly but we can shape it later

--- sudoku.py
+++ sudoku.py
@@ -3,6 +3,7 @@ from board import Board
 from cell import Cell
 import pygame
 
+
 class Sudoku:
 
     def __init__(self):
@@ -66,9 +67,71 @@ class Sudoku:
         new_board = Board(9, 9, self.screen, level)
         game_play_on = True
         while game_play_on:
-            
+
             new_board.draw()
             pygame.display.flip()
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_posx = mouse_pos[0]
+            mouse_posy = mouse_pos[1]
+
+            ##Code below for interactive elements
+            for eve in pygame.event.get():
+
+                if eve.type == pygame.MOUSEBUTTONDOWN:
+                    cell_chose = []
+                    cell_chose = new_board.click(mouse_posx, mouse_posy)
+                    cell_chosenx = cell_chose[0]
+                    cell_choseny = cell_chose[1]
+
+                    if cell_chose != None:
+                        new_board.select(cell_chosenx, cell_choseny)
+                        #print(new_board.board)
+
+                if eve.type == pygame.KEYDOWN:
+                    cell_chose = list(cell_chose)
+                    #print(cell_chose)
+
+                    if eve.key == pygame.K_1:
+                        new_cell = Cell(1,cell_chosenx, cell_choseny,self.screen)
+                        new_cell.draw()
+                        pygame.display.flip()
+                    elif eve.key == pygame.K_2:
+                        new_cell = Cell(2, cell_chosenx, cell_choseny, self.screen)
+                        new_cell.draw()
+                        pygame.display.flip()
+                    elif eve.key == pygame.K_3:
+                        new_cell = Cell(3, cell_chosenx, cell_choseny, self.screen)
+                        new_cell.draw()
+                        pygame.display.flip()
+                    elif eve.key == pygame.K_4:
+                        new_cell = Cell(4, cell_chosenx, cell_choseny, self.screen)
+                        new_cell.draw()
+                        pygame.display.flip()
+                    elif eve.key == pygame.K_5:
+                        new_cell = Cell(5, cell_chosenx, cell_choseny, self.screen)
+                        new_cell.draw()
+                        pygame.display.flip()
+                    elif eve.key == pygame.K_6:
+                        new_cell = Cell(6, cell_chosenx, cell_choseny, self.screen)
+                        new_cell.draw()
+                        pygame.display.flip()
+                    elif eve.key == pygame.K_7:
+                        new_cell = Cell(7, cell_chosenx, cell_choseny, self.screen)
+                        new_cell.draw()
+                        pygame.display.flip()
+                    elif eve.key == pygame.K_8:
+                        new_cell = Cell(8, cell_chosenx, cell_choseny, self.screen)
+                        new_cell.draw()
+                        pygame.display.flip()
+                    elif eve.key == pygame.K_9:
+                        new_cell = Cell(9, cell_chosenx, cell_choseny, self.screen)
+                        new_cell.draw()
+                        pygame.display.flip()
+
+
+
+
+
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
